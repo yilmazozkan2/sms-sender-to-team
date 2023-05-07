5745f009b8363e9018356a56a1e85ffeef96c176
Ozkan Yilmaz <ydkhspl21@gmail.com>
2023-05-07 22:59:58 +0300

    added expo-sms

--- src/navigation/screens/AddPersonScreen.js
+++ src/navigation/screens/AddPersonScreen.js
@@ -1,13 +1,13 @@
-import { View, TextInput, Button, Alert } from 'react-native'
+import { View, TextInput, Text, Alert, TouchableOpacity } from 'react-native'
 import React, { useState } from 'react'
 import Realm from 'realm';
+import PhoneInput from 'react-native-phone-number-input';
 
 export default function AddPersonScreen() {
   const [name, setName] = useState("");
   const [phone, setPhone] = useState("");
   let realm = new Realm();
-
-
+  
   var ID = realm.objects('Person_Info').length + 1;
   const values = {
     id: ID,
@@ -20,13 +20,24 @@ export default function AddPersonScreen() {
     });
     Alert.alert("Kişi Başarıyla Eklendi.")
   }
-
   return (
     <View>
-      <TextInput style={{ backgroundColor: 'white', color: 'black' }} placeholder='İsim Giriniz' placeholderTextColor='black' onChangeText={setName} />
-      <TextInput style={{ backgroundColor: 'white', color: 'black' }} placeholder='Telefon Numarası Giriniz' placeholderTextColor='black' keyboardType='phone-pad' onChangeText={setPhone} />
-      <Button title='Ekle' onPress={addPerson} />
-
+      <TextInput style={{ backgroundColor: 'white', color: 'black', paddingLeft: 10, paddingTop: 10 }} placeholder='enter name' placeholderTextColor='black' onChangeText={setName} />
+      {/* <TextInput  style={{ backgroundColor: 'white', color: 'black', paddingLeft: 10, paddingTop:10 }} placeholder='enter phone number' placeholderTextColor='black' keyboardType='phone-pad' defaultValue='+' onChangeText={setPhone} /> */}
+      
+      <PhoneInput
+      defaultValue={phone}
+      defaultCode='TR'
+      withShadow
+      onChangeFormattedText={(e)=>{
+        setPhone(e)
+      }}
+      />
+      
+      
+      <TouchableOpacity onPress={addPerson} style={{alignItems: 'center', justifyContent: 'center', marginLeft: 15, marginTop:15, backgroundColor: 'aqua', width: 100, height: 40 }}>
+        <Text style={{fontSize: 20}}>Add</Text>
+      </TouchableOpacity>
     </View>
   )
 }
--- src/navigation/screens/SendScreen.js
+++ src/navigation/screens/SendScreen.js
@@ -1,34 +1,47 @@
 import { View, Text, TextInput, Button, Alert } from 'react-native'
 import React, { useEffect, useState } from 'react'
 import Realm from 'realm';
+import * as SMS from 'expo-sms';
+
+
 export default function SendScreen({ route }) {
   const { name, phone, id } = route.params.item
   const [message, setMessage] = useState('');
   const [personID, setPersonID] = useState('');
+  const [isAvailable, setIsAvailable] = useState(false);
+  const [phones, setPhones] = useState([]);
   let realm = new Realm();
   useEffect(() => {
-
     setPersonID(id)
-  }, [])
 
+    checkAvailability();
+  }, [])
+  const checkAvailability = async () => {
+    const isSmsAvailable = await SMS.isAvailableAsync();
+    setIsAvailable(isSmsAvailable);
+  }
   const deletePerson = () => {
     realm.write(() => {
       realm.delete(realm.objects('Person_Info').filtered(`id = ${personID}`))
-      
+
     });
     Alert.alert('Kişi Başarıyla Silindi.');
   }
+  const sendSMS = async () => {
+    const { result } = await SMS.sendSMSAsync(
+      phone, message
+    )
+  }
   return (
     <View>
+      {isAvailable ? <Button title='Gönder' onPress={sendSMS} /> : <Text>SMS not available</Text>}
       <Text>{name}</Text>
-      <Text>{phone}</Text>
+      <TextInput defaultValue={phone}/>
       <TextInput
         placeholder='Mesajınızı Giriniz.'
         style={{ backgroundColor: 'aqua', color: 'black' }}
+        value={message}
         onChangeText={(e) => setMessage(e)}
-      />
-      <Button
-        title='Gönder'
       />
       <Button
         title='Kişiyi Sil'
