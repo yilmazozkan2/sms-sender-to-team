ca2f17a43f3a5699a84bef98e33454ccbdffe1d7
Ozkan Yilmaz <ydkhspl21@gmail.com>
2023-05-11 19:26:34 +0300

    deleted tests

--- __tests__/NameInputComponent.test.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { shallow } from 'enzyme';
-import NameInputComponent from '../src/components/AddScreen/NameInputComponent';
-
-import { expect } from 'chai';
-
